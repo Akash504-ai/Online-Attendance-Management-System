ddc544ad306daf2d8d1b733e1e95a9678cb8e5d4
Akash504-ai <santraakash999@gmail.com>
2025-11-12 01:51:48 +0530

    frontend connect

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost/Trackify/backend'
+const API_BASE_URL = 'https://online-attendance-management-system-1.onrender.com/';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
